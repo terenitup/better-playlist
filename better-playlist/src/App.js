75d04e76bc9872695d352d466668ba0200e19d48
terenitup <terentaniuchi@gmail.com>
2018-08-31 11:45:28 -0600

    filter applies to the playlists display and the hours display as well

--- better-playlist/src/App.js
+++ better-playlist/src/App.js
@@ -116,13 +116,11 @@ class App extends Component {
 
   render() {
     
-  //   let playlistElements = []
-  //   if (this.state.serverData.user) {
-  //     for (let i = 0; i < this.state.serverData.user.playlists.length; i++) {
-  //       let playlist = this.state.serverData.user.playlists[i]
-  //       playlistElements.push(<Playlist playlist={playlist} />)
-  //   }
-  // }
+    let playlistToRender = this.state.serverData.user ? this.state.serverData.user.playlists
+      .filter(playlist =>
+        playlist.name.toLowerCase().includes(
+          this.state.filterString.toLowerCase())
+    ) : []
         
     return (
       <div className="App">
@@ -133,16 +131,13 @@ class App extends Component {
               {this.state.serverData.user.name}'s Playlist
             </h1>
             
-            <PlaylistCounter playlists={this.state.serverData.user.playlists} />
-            <HoursCounter playlists={this.state.serverData.user.playlists} />
+            <PlaylistCounter playlists={playlistToRender} />
+            <HoursCounter playlists={playlistToRender} />
               
             <Filter onTextChange={text => {
                 this.setState({filterString: text})} 
               }/>
-            {this.state.serverData.user.playlists.filter(playlist =>
-              playlist.name.toLowerCase().includes(
-                this.state.filterString.toLowerCase())
-            ).map(playlist => 
+            {playlistToRender.map(playlist => 
               <Playlist playlist={playlist} />
             )}
           </div> : <h1 style={defaultStyle}>Loading...</h1>
